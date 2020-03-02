17beb63ec90417087afc871d8e6b709b7426b72b
Yusuf Umut Bulak <46137045+ysumut@users.noreply.github.com>
2020-03-03 00:27:57 +0300

    Senkronizasyon kontrolü yapıldı 💫

--- src/controller/example_cont.js
+++ src/controller/example_cont.js
@@ -8,8 +8,7 @@ class example_cont{
     
     async render(req,res){
         let data = await this.example_func(req);
-        
-        res.write("<h1> Your APP! </h1><h4>It is an example page.</h4> <p>Results: " + data.toString() + "</p>");
+        res.write("<h1> WEB APP! </h1><h4>It is an example page.</h4> <p>Results: " + data.toString() + "</p>");
         res.end();
 
     }
