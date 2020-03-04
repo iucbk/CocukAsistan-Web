d9d175d5d3607172f89263639bf0831ea119bc79
ysumut <umut_aslan95@hotmail.com>
2020-03-05 02:27:01 +0300

    Model fonksiyonu düzenlendi.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", routeManager);
 
--- src/model/register_model.js
+++ src/model/register_model.js
@@ -4,12 +4,12 @@ const conn = require('../config/db');
 function registerUser(body){
     
     return new Promise(resolve => {
-        let query = "INSERT INTO user (email, full_name, password) VALUES ('?','?','?')";
+        let query = "INSERT INTO user (email, full_name, password) VALUES (?,?,?)";
 
         conn.query(query, [body.email, body.full_name, body.password], (err) => {
                 let message = "";
             
-                if (err) message = "error";
+                if (err){ message = "error";}
                 else message = "success";
                 
                 resolve({
