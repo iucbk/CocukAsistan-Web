bad713f93a6f1c724f28378cebd97baceddb1638
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-03-05 15:08:58 +0300

    JWT kontrolü middleware olarak eklendi ➰ #12

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const routeManager = require("./config/routeManager");
+const verifyAuth = require("./middleware/verify_auth");
+
 var bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
+app.use(verifyAuth);
 app.use("/", routeManager);
 app.listen(8080, () => {
   console.log("Your app is ready at 8080 port.");
