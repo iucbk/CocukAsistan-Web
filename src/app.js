8c315c81b4bdbf45bb4efaa26853f9650183d877
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-03-05 14:02:42 +0300

    Login işlemi tamamlamndı 🚪 (#14)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const routeManager = require('./config/routeManager');
+const express = require("express");
+const routeManager = require("./config/routeManager");
+var bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
 
-app.use('/', routeManager);
-
-
-app.listen(8080, ()=>{
-    console.log("Your app is ready at 8080 port.");
+app.use("/", routeManager);
+app.listen(8080, () => {
+  console.log("Your app is ready at 8080 port.");
 });
