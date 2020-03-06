bf269a8a860ff65ab642296e07bdbe2a08fc7a2c
Yasin Osman <yasinosman10@gmail.com>
2020-03-06 17:12:29 +0300

    Add /quiz/search route
    routea yapılan requestin body kısmındaki category_id verisi kullanılarak databasede arama yapılır. bulunan sonuçlar kullanıcıya results adı altında döner.

--- src/config/routeManager.js
+++ src/config/routeManager.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const loginController = require('../controller/login_cont');
 const quizCategories = require('../controller/quiz_categories_cont');
+const verifyAuth = require('../middleware/verify_auth');
+const mysql = require('mysql');
 
 
 router.get('/example', async (req, res) => {
@@ -32,5 +34,29 @@ router.get('/finishedquizzes', (req, res) => {
     // code
 });
 
-
+router.post('/quizes/search', verifyAuth,  async (req, res)=>{
+    //connection yarat
+    const connection = mysql.createConnection({
+        host: 'cocukasistanmysql.mysql.database.azure.com',
+        user: 'asistan@cocukasistanmysql',
+        password: 'iucbkCocuk2020',
+        database: 'cocukasistan',
+        port: 3306
+    });
+    //req.category_id yi kullanarak databasede arama yap
+    const q = "SELECT quiz_title FROM Quiz WHERE category_id='";
+    try{
+        connection.query(q+req.body.category_id+"';", (error, results)=>{
+            if(error){
+                res.status(400).json({err:error});
+            } else {
+//Response olarak ise databaseden quiz_title verilerini gönder.
+                res.status(200).json({results:results});
+            }
+        });
+    } catch (err){
+        console.log(err);
+        res.status(500).send({err:err});
+    }
+});
 module.exports = router;
