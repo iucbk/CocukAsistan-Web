3f381e4729d0f79693f0a87e48cd8a4fe8e58209
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-03-07 22:48:57 +0300

    RouteManager yapısı düzeltildi 👩‍🔧 (#34)

--- src/config/routeManager.js
+++ src/config/routeManager.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const loginController = require('../controller/login_cont');
+const registerController = require('../controller/register_cont');
 const quizCategories = require('../controller/quiz_categories_cont');
 const quizById = require("../controller/quiz_by_id_cont");
 const objectById = require('../controller/object_by_id_cont');
@@ -11,30 +12,27 @@ router.get('/example', async (req, res) => {
 });
 
 
-router.get('/register', (req, res) => {
+// user router
+router.post('/user/signup', registerController.register);
 
-    // code
-});
-
-
-router.post('/login', loginController.login);
-
-router.get('/getQuizCategories', quizCategories.quizCategories);
+router.post("/user/login", loginController.login);
 
 
-router.get('/newquiz', (req, res) => {
-
-    // code
-});
-
-
-router.get('/finishedquizzes', (req, res) => {
-
-    // code
-});
-
-router.get('/object/getById', objectById.objectById);
+// quiz router
+router.get("/quiz/getCategories", quizCategories.quizCategories);
 
 router.get("/quiz/getById", quizById.quizById);
+
+
+// object router
+router.get('/object/getById', objectById.objectById);
+
+router.get("/newquiz", (req, res) => {
+  // code
+});
+
+router.get("/finishedquizzes", (req, res) => {
+  // code
+});
 
 module.exports = router;
