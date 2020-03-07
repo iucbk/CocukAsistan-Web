ee1d61b6f5ef4e427c949745c77e2d8385861a9b
ysumut <umut_aslan95@hotmail.com>
2020-03-08 02:06:37 +0300

    Login error yapıları düzenlendi

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 const config =
 {
-    host: 'cocukasistanmysql.mysql.database.azure.com',
-    user: 'asistan@cocukasistanmysql',
-    password: 'iucbkCocuk2020',
+    host: 'localhost',
+    user: 'root',
+    password: '',
     database: 'cocukasistan',
     port: 3306
 };
