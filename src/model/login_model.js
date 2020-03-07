bb2558c7bcff35f232eb1d5b9c384bc4783a076c
Yusuf Umut Bulak <46137045+ysumut@users.noreply.github.com>
2020-03-07 22:12:52 +0300

    Şifreleme fonksiyonelliği login yapısına eklendi 🔏 (#33)

--- src/model/login_model.js
+++ src/model/login_model.js
@@ -3,8 +3,8 @@ const conn = require("../config/db");
 function login(body) {
   return new Promise(resolve => {
     let query =
-      "SELECT * FROM cocukasistan.user WHERE email = ? AND password = ?";
-    conn.query(query, [body.email, body.password], (err, results, fields) => {
+      "SELECT * FROM cocukasistan.user WHERE email = ?";
+    conn.query(query, [body.email], (err, results, fields) => {
       if (err) throw err;
       resolve(results);
     });
