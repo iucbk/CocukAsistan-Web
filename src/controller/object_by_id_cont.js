4e255b5c4966ef9c1830b375e6abbdd86767dd0d
Yasin Osman <yasinosman10@gmail.com>
2020-03-07 22:40:16 +0300

    Object By Id arama fonksiyonelliği eklendi 🔍 (#31)

---
+++ src/controller/object_by_id_cont.js
@@ -0,0 +1,7 @@
+const objectByIdModel = require("../model/object_by_id_model");
+const resFun = require("../utils/response_functions");
+
+exports.objectById = async (req, res) => {
+    let object = await objectByIdModel(req.body.object_id);
+    res.status(200).json(resFun.success(200, "Object fetched successfully", object));
+};
