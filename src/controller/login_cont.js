0a23b9f0aed17ee7a2a759bc8efa894335b37cc7
Yusuf Umut Bulak <46137045+ysumut@users.noreply.github.com>
2020-03-08 09:38:20 +0300

    Login Error Response Yapıları Düzenlendi 👨‍🔧 (#35)

--- src/controller/login_cont.js
+++ src/controller/login_cont.js
@@ -7,27 +7,20 @@ function comparePassword(password, hash){
     return new Promise(resolve => {
 
         bcrypt.compare(password, hash, (err, res) => {
-            let compare_error = 0;
-            let is_compare = 0;
-            
             if(err){
-                compare_error = 1;
                 resolve({
-                    compare_error: compare_error
+                    compare_error: 1
                 });
-                return;
             }
 
             if(res) {
-                is_compare = 1;
                 resolve({
-                    is_compare: is_compare
+                    is_compare: 1
                 });
             } 
             else {
-                isCompare = 0;
                 resolve({
-                    is_compare: is_compare
+                    is_compare: 0
                 });
             } 
             
@@ -40,10 +33,16 @@ PRIVATE_KEY = "COCUK_ASISTAN_2020_PRIVATE_KEY";
 
 exports.login = async (req, res) => {
     let data = await loginModel(req.body);
-    let compare = await comparePassword(req.body.password, data[0].password);
+
+    if(data.db_error){
+        res.status(503).json(resFun.fail(503, "Database error"));
+        return;
+    }
     
     if (data.length == 1) {
-        jwt.sign({ id: data[0].user_id }, PRIVATE_KEY, function (err, token) {
+        let compare = await comparePassword(req.body.password, data[0].password);
+
+        jwt.sign({ id: data[0].user_id }, PRIVATE_KEY, (err, token) => {
             if (err) {
                 res.status(500).json(resFun.fail(500, "An error occured while creating token"))
             };
@@ -54,8 +53,7 @@ exports.login = async (req, res) => {
             if(compare.is_compare)
                 res.status(200).json(resFun.success(200, "Logged in successfully", { token: token }));
             else
-                res.status(422).json(resFun.fail(422, "Password did not match"));
-
+                res.status(422).json(resFun.fail(422, "Incorrect email or password"));
         });
     } else {
         res.status(422).json(resFun.fail(422, "Incorrect email or password"));
--- src/model/login_model.js
+++ src/model/login_model.js
@@ -1,11 +1,17 @@
 const conn = require("../config/db");
 
 function login(body) {
+
   return new Promise(resolve => {
-    let query =
-      "SELECT * FROM cocukasistan.user WHERE email = ?";
+    let query = "SELECT * FROM cocukasistan.user WHERE email = ?";
+
     conn.query(query, [body.email], (err, results, fields) => {
-      if (err) throw err;
+
+      if (err){
+        resolve({
+          db_error: 1
+        });
+      }
       resolve(results);
     });
   });
