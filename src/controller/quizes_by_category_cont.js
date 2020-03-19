b4cced9f9f3dd26c99c9a64220a3589b1845ade4
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-03-19 16:49:05 +0300

    category_id istek query'ye eklendi 👩‍🔧

--- src/controller/quizes_by_category_cont.js
+++ src/controller/quizes_by_category_cont.js
@@ -2,7 +2,7 @@ const quizesByCategoryModel = require("../model/quizes_by_category_model");
 const resFun = require("../utils/response_functions");
 
 exports.quizesByCategory = async (req, res) => {
-    let data = await quizesByCategoryModel(req.body.decoded_id, req.body.category_id);
+    let data = await quizesByCategoryModel(req.body.decoded_id, req.query.category_id);
     if(data.dbError){
         res.status(503).json(resFun.fail(503, "Database error"));
         return;
