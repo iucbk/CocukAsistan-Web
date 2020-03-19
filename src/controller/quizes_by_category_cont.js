8991521b4370ac2bc1b1b6fe4ffd4e2173fc007a
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-03-19 16:50:22 +0300

    Kategori ile quizleri çekme tamamlandı 👩‍🚀 (#51)

---
+++ src/controller/quizes_by_category_cont.js
@@ -0,0 +1,11 @@
+const quizesByCategoryModel = require("../model/quizes_by_category_model");
+const resFun = require("../utils/response_functions");
+
+exports.quizesByCategory = async (req, res) => {
+    let data = await quizesByCategoryModel(req.body.decoded_id, req.query.category_id);
+    if(data.dbError){
+        res.status(503).json(resFun.fail(503, "Database error"));
+        return;
+    }
+    res.status(200).json(resFun.success(200, "Quizes fetched successfully", data.quizes));
+};
